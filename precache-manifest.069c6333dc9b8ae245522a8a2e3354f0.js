0fbe0edbfea85df76acae785b0bd0e89ea4c2f83
karina1208 <56858132+karina1208@users.noreply.github.com>
2021-02-05 13:10:33 +0800

    Updates

---
+++ precache-manifest.069c6333dc9b8ae245522a8a2e3354f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a37ee139d79434ef6c7",
+    "url": "/saitoasuka-info/static/css/main.25901a20.chunk.css"
+  },
+  {
+    "revision": "6a37ee139d79434ef6c7",
+    "url": "/saitoasuka-info/static/js/main.3dfc263b.chunk.js"
+  },
+  {
+    "revision": "51034d3422329ca60942",
+    "url": "/saitoasuka-info/static/js/runtime~main.b410ceae.js"
+  },
+  {
+    "revision": "b5b99cf708d8e7183e4b",
+    "url": "/saitoasuka-info/static/css/2.6fd7434b.chunk.css"
+  },
+  {
+    "revision": "b5b99cf708d8e7183e4b",
+    "url": "/saitoasuka-info/static/js/2.759d05c5.chunk.js"
+  },
+  {
+    "revision": "bc4dc2f569fa3249aaa5",
+    "url": "/saitoasuka-info/static/js/3.8bfb7909.chunk.js"
+  },
+  {
+    "revision": "325cafc06e97aab5cde03c883bb5c797",
+    "url": "/saitoasuka-info/index.html"
+  }
+];
